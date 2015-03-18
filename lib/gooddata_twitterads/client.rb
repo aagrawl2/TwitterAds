49b4a8fffb480f5ae9a5f5f4b1741eac963e4330
Akshay Agrawal <akshay.agrawal1101@gmail.com>
2015-03-18 09:22:24 -0700

    Update client.rb

--- lib/gooddata_twitterads/client.rb
+++ lib/gooddata_twitterads/client.rb
@@ -12,13 +12,14 @@ require_relative 'models/stats_promoted_tweets'
 
 class GoodDataTwitterAds::Client
 
-	attr_reader :fresh_token
 	def initialize config={}
 		consumer_key        = config.delete(:consumer_key)
 		consumer_secret     = config.delete(:consumer_secret)
 		access_token        = config.delete(:access_token)
 		access_token_secret = config.delete(:access_token_secret)
 		@fresh_token        = create_token(consumer_key,consumer_secret,access_token,access_token_secret)
+		@counter            = 0 # This is used to see if we are making 1800 API calls per 15 min window
+		@Timer              = Time.now
 	end
 
 	def accounts
@@ -42,6 +43,21 @@ class GoodDataTwitterAds::Client
 	end
 
 
+	def fresh_token()
+		current_time = Time.now
+		if @counter >1800 and (current_time - @Timer)<800 # 800 refers to seconds i.e. 13 mintutes 
+			puts "We have exceeded 1800 API calls per 15 minute window. Sleeping"
+			sleep_time  = 900 - (current_time - @Timer)
+			sleep(sleep_time) # This is calculated so that we sleep for 15 minutes - (time we have spent uptil now)
+			puts "Resetting counters and timer"
+			@counter = 0 # Resetting counter
+			@Timer = Time.now # Resetting Time
+		end
+		@counter = @counter +1
+		puts "API call = #{@counter}"
+		return @fresh_token
+	end
+
 	private
 	def create_token(consumer_key,consumer_secret,access_token,access_token_secret)
 		if consumer_key.nil? || consumer_secret.nil? || access_token.nil? || access_token_secret.nil?
@@ -55,5 +71,3 @@ class GoodDataTwitterAds::Client
 
 
 end
-
-
